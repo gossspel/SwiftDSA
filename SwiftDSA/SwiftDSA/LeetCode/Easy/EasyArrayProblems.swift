32aeb8be271633038495eb5f4fdbfd368959e455
Sunny Chan <sunnyskchan@berkeley.edu>
2021-06-07 12:25:20 -0700

    Added some updates.

---
+++ SwiftDSA/SwiftDSA/LeetCode/Easy/EasyArrayProblems.swift
@@ -0,0 +1,47 @@
+//
+//  EasyArrayProblems.swift
+//  SwiftDSA
+//
+//  Created by Sunny Chan on 6/7/21.
+//  Copyright © 2021 Sunny Chan. All rights reserved.
+//
+
+import Foundation
+
+class EasyArrayProblems {}
+
+// MARK: - 26. Remove Duplicates from Sorted Array
+// LINK: https://leetcode.com/problems/remove-duplicates-from-sorted-array/
+//
+// Description: Given a sorted array nums, remove the duplicates in-place such that each element appears only once and
+// returns the new length. Do not allocate extra space for another array, you must do this by modifying the input array
+// in-place with O(1) extra memory.
+//
+// Strategy: Have four vars prev: Int? = nil, current: Int? = nil, maxLength: Int = nums.count,
+// numberOfProcessedElement: Int = 0, While (numberOfProcessedElement < maxLength) loop throught the nums array. During
+// each iteration, update current with the current value of the loop, compare it with prev, if it is the same, remove
+// it and decrement maxLength. At the end of each iteration, update prev with current, increment
+// numberOfProcessedElement.
+
+extension EasyStringProblems {
+    static func removeDuplicates(_ nums: inout [Int]) -> Int {
+        var prev: Int? = nil
+        var current: Int? = nil
+        var maxLength: Int = nums.count
+        var numberOfProcessedElement: Int = 0
+        
+        while numberOfProcessedElement < maxLength {
+            current = nums[numberOfProcessedElement]
+            
+            if current == prev {
+                let _ = nums.remove(at: numberOfProcessedElement)
+                maxLength -= 1
+            }
+            
+            prev = current
+            numberOfProcessedElement += 1
+        }
+        
+        return nums.count
+    }
+}
---
+++ SwiftDSA/SwiftDSATests/LeetCode/Easy/EasyArrayProblemsTests.swift
@@ -0,0 +1,20 @@
+//
+//  EasyArrayProblemsTests.swift
+//  SwiftDSATests
+//
+//  Created by Sunny Chan on 6/7/21.
+//  Copyright © 2021 Sunny Chan. All rights reserved.
+//
+
+@testable import SwiftDSA
+import XCTest
+
+class EasyArrayProblemsTests: XCTestCase {}
+
+// MARK: - 26. Remove Duplicates from Sorted Array
+
+extension EasyArrayProblemsTests {
+    func testRemoveDuplicates_withSortedArrayWithOneDuplicatedElement_shouldReturnTwo() {
+        let nums: [Int] = [1, 1, 2]
+    }
+}
