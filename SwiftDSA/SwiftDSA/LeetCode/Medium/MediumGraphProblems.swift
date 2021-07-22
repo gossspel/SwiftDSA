445f341bb2a99b44bc8833b19804a80488da1c96
Sunny Chan <sunnyskchan@berkeley.edu>
2021-07-22 11:24:27 -0700

    Added more updates.

--- SwiftDSA/SwiftDSA/LeetCode/Medium/MediumGraphProblems.swift
+++ SwiftDSA/SwiftDSA/LeetCode/Medium/MediumGraphProblems.swift
@@ -26,7 +26,8 @@ class MediumGraphProblems {}
 // Each node in the graph contains a value (int) and a list (List[Node]) of its neighbors.
 //
 // Strategy: Use BFS, use a nodeByValue: [Int: Node] to store the copied node. Use BFS to copy the nodes and query the
-// nodeByValue to update the neighbors on each node. return nodeByValue[firstNodeValue] in the end.
+// nodeByValue to update the neighbors on each node. return nodeByValue[firstNodeValue] in the end. Alternatively, we
+// can also use DFS.
 
 extension MediumGraphProblems {
     func cloneGraph(_ node: Node?) -> Node? {
@@ -64,6 +65,42 @@ extension MediumGraphProblems {
             }
             
             currentNodes = nextNodes
+        }
+        
+        return nodeByValue[sureNode.val]
+    }
+    
+    func cloneGraphDFS(_ node: Node?) -> Node? {
+        guard let sureNode = node else {
+            return nil
+        }
+        
+        var nodeByValue: [Int: Node] = [sureNode.val: Node(sureNode.val)]
+        var currentNodes: [Node] = [sureNode]
+        
+        while !currentNodes.isEmpty {
+            let currentNode = currentNodes.removeLast()
+            
+            if let cachedNode = nodeByValue[currentNode.val], !cachedNode.neighbors.isEmpty {
+                // The node has already been copied, skip the copy process
+                continue
+            } else {
+                for neighbor in currentNode.neighbors {
+                    guard let neighborToCopy = neighbor else {
+                        continue
+                    }
+                    
+                    // copy the neighbor, and save the neighbor into the dictionary if we haven't done so
+                    if let cachedNeighbor = nodeByValue[neighborToCopy.val] {
+                        nodeByValue[currentNode.val]?.neighbors.append(cachedNeighbor)
+                    } else {
+                        let copiedNeighbor = Node(neighborToCopy.val)
+                        nodeByValue[currentNode.val]?.neighbors.append(copiedNeighbor)
+                        nodeByValue[neighborToCopy.val] = copiedNeighbor
+                        currentNodes.append(neighborToCopy)
+                    }
+                }
+            }
         }
         
         return nodeByValue[sureNode.val]
