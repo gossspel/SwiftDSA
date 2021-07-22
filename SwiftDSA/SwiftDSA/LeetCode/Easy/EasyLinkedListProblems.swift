4683a48b7e507a7e4d8fee12e404390b0a3a7667
Sunny Chan <sunnyskchan@berkeley.edu>
2021-07-21 17:37:55 -0700

    Added more updates.

--- SwiftDSA/SwiftDSA/LeetCode/Easy/EasyLinkedListProblems.swift
+++ SwiftDSA/SwiftDSA/LeetCode/Easy/EasyLinkedListProblems.swift
@@ -10,6 +10,64 @@ import Foundation
 
 class EasyLinkedListProblems {}
 
+// MARK: - 141. Linked List Cycle
+// LINK: https://leetcode.com/problems/linked-list-cycle/
+// VIDEO: https://www.youtube.com/watch?v=gBTe7lFR3vc
+//
+// Description: Given head, the head of a linked list, determine if the linked list has a cycle in it. There is a cycle
+// in a linked list if there is some node in the list that can be reached again by continuously following the next
+// pointer. Internally, pos is used to denote the index of the node that tail's next pointer is connected to. Note that
+// pos is not passed as a parameter. Return true if there is a cycle in the linked list. Otherwise, return false.
+//
+// Strategy: Loop through the linked list, record the memory address of every node in a set. If the set already
+// contains that memory address, return true, otherwise continue until we get to a node in which node.next == nil and
+// return false.
+//
+// Optimal Strategy: Use Floyd's tortoise and hare algorithm and return true if slow === fast at some point. This is
+// because the fast pointer will always catch up with the slow pointer at some point if a cycle exists. For example, if
+// there is 5 nodes between fast and slow pointer, at the next iteration there will be (5 + 1 - 2) = 4 nodes between
+// fast and slow pointer. Effectively, the distance between fast and slow pointers is decreased by 1 node at each
+// iteration.
+
+extension EasyLinkedListProblems {
+    func hasCycle(_ head: ListNode?) -> Bool {
+        var fast: ListNode? = head?.next?.next
+        var slow: ListNode? = head?.next
+        var iterationCount: Int = 0
+
+        while let sureFast = fast, let sureSlow = slow {
+            if sureFast === sureSlow {
+                return true
+            }
+
+            fast = sureFast.next?.next
+            slow = sureSlow.next
+            iterationCount += 1
+        }
+
+        return false
+    }
+    
+    func hasCycleSlow(_ head: ListNode?) -> Bool {
+        var memAddressSet: Set<String> = Set()
+        var current: ListNode? = head
+        
+        while let sureCurrent = current {
+            let memAddress: String = "\(Unmanaged.passUnretained(sureCurrent).toOpaque())"
+            
+            if memAddressSet.contains(memAddress) {
+                return true
+            } else {
+                memAddressSet.insert(memAddress)
+            }
+            
+            current = sureCurrent.next
+        }
+        
+        return false
+    }
+}
+
 // MARK: - 206. Reverse Linked List
 // LINK: https://leetcode.com/problems/reverse-linked-list/
 //
--- SwiftDSA/SwiftDSATests/LeetCode/Easy/EasyLinkedListProblemsTests.swift
+++ SwiftDSA/SwiftDSATests/LeetCode/Easy/EasyLinkedListProblemsTests.swift
@@ -9,10 +9,52 @@
 import XCTest
 @testable import SwiftDSA
 
+// MARK: - 141. Linked List Cycle
+
+class EasyLinkedListProblem141Tests: XCTestCase {
+    func testHasCycle_with4CyclicalNodes_shouldReturnTrue() {
+        let node3 = ListNode(-4)
+        let node2 = ListNode(0, node3)
+        let node1 = ListNode(2, node2)
+        let root = ListNode(3, node1)
+        node3.next = root
+        XCTAssertTrue(EasyLinkedListProblems().hasCycle(root))
+    }
+    
+    func testHasCycle_with4NonCyclicalNodes_shouldReturnFalse() {
+        let node3 = ListNode(-4)
+        let node2 = ListNode(0, node3)
+        let node1 = ListNode(2, node2)
+        let root = ListNode(-4, node1)
+        XCTAssertFalse(EasyLinkedListProblems().hasCycle(root))
+    }
+    
+    func testHasCycle_with2CyclicalNodes_shouldReturnTrue() {
+        let node1 = ListNode(2)
+        let root = ListNode(1, node1)
+        node1.next = root
+        XCTAssertTrue(EasyLinkedListProblems().hasCycle(root))
+    }
+    
+    func testHasCycle_with1CyclicalNode_shouldReturnTrue() {
+        let root = ListNode(1)
+        root.next = root
+        XCTAssertTrue(EasyLinkedListProblems().hasCycle(root))
+    }
+    
+    func testHasCycle_with1NonCyclicalNode_shouldReturnFalse() {
+        let root = ListNode(1)
+        XCTAssertFalse(EasyLinkedListProblems().hasCycle(root))
+    }
+    
+    func testHasCycle_with0CyclicalNodes_shouldReturnfalse() {
+        XCTAssertFalse(EasyLinkedListProblems().hasCycle(nil))
+    }
+}
+
 // MARK: - 206. Reverse Linked List
 
 class EasyLinkedListProblem206Tests: XCTestCase {
-
     func testReverseList_with5Nodes_shouldReturnReversedList() {
         let node5 = ListNode(5)
         let node4 = ListNode(4, node5)
