8007390bf39e50ed2aaa764b48dcbf64f4143449
Sunny Chan <sunnyskchan@berkeley.edu>
2021-08-01 10:59:17 -0700

    Added more updates.

--- SwiftDSA/SwiftDSA/LeetCode/Easy/EasyStringProblems.swift
+++ SwiftDSA/SwiftDSA/LeetCode/Easy/EasyStringProblems.swift
@@ -51,3 +51,36 @@ extension EasyStringProblems {
     }
 }
 
+// MARK: - 242. Valid Anagram
+// LINK: https://leetcode.com/problems/valid-anagram/
+//
+// Description: Given two strings s and t, return true if t is an anagram of s, and false otherwise.
+//
+// Strategy: Create a dict called occurrenceByChar: [Char: Int]. Loop through the first string, update occurrenceByChar
+// by incrementing the occurrence. After that, loop through the second string, update occurrenceByChar by decrementing
+// the occurrence. In the end, loop through the dict and make sure the occurrence of any char is 0.
+
+extension EasyStringProblems {
+    func isAnagram(_ s: String, _ t: String) -> Bool {
+        var occurrenceByChar: [Character: Int] = [:]
+        let sChars: [Character] = Array(s)
+        
+        for char in sChars {
+            occurrenceByChar[char] = (occurrenceByChar[char] ?? 0) + 1
+        }
+        
+        let tChars: [Character] = Array(t)
+        
+        for char in tChars {
+            occurrenceByChar[char] = (occurrenceByChar[char] ?? 0) - 1
+        }
+        
+        for (_, value) in occurrenceByChar {
+            if value != 0 {
+                return false
+            }
+        }
+        
+        return true
+    }
+}
