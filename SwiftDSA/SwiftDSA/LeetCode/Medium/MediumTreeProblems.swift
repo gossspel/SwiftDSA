93650352441f016160a826c761ae7628ecd4e824
Sunny Chan <sunnyskchan@berkeley.edu>
2021-08-24 11:36:11 -0700

    Added more updates.

--- SwiftDSA/SwiftDSA/LeetCode/Medium/MediumTreeProblems.swift
+++ SwiftDSA/SwiftDSA/LeetCode/Medium/MediumTreeProblems.swift
@@ -53,3 +53,26 @@ extension MediumTreeProblems {
         return results
     }
 }
+
+// MARK: - 105. Construct Binary Tree from Preorder and Inorder Traversal
+// LINK: https://leetcode.com/problems/construct-binary-tree-from-preorder-and-inorder-traversal/
+//
+// Description: Given two integer arrays preorder and inorder where preorder is the preorder traversal of a binary tree
+// and inorder is the inorder traversal of the same tree, construct and return the binary tree.
+//
+// Strategy: Notice that the first node of preOrder array is always the root node. Also notice that the nodes that come
+// before the root node in inOrder array are nodes of the left subtree, and the nodes that come after the root node are
+// nodes of the right subtree. With these information, we can recursively recreate the whole tree.
+
+extension MediumTreeProblems {
+    func buildTree(_ preorder: [Int], _ inorder: [Int]) -> TreeNode? {        
+        guard let rootValue = preorder.first, let rootIOIndex = inorder.firstIndex(of: rootValue) else {
+            return nil
+        }
+        
+        let root = TreeNode(rootValue)
+        root.left = buildTree(Array(preorder[1..<(1 + rootIOIndex)]), Array(inorder[0..<rootIOIndex]))
+        root.right = buildTree(Array(preorder[(1 + rootIOIndex)...]), Array(inorder[(rootIOIndex + 1)...]))
+        return root
+    }
+}
