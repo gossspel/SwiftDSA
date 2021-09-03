440fd226eeed2502ff64498609a5b43ad180c079
Sunny Chan <sunnyskchan@berkeley.edu>
2021-09-03 13:40:29 -0700

    Added more updates.

--- SwiftDSA/SwiftDSA/LeetCode/Medium/MediumDynamicProgrammingProblems.swift
+++ SwiftDSA/SwiftDSA/LeetCode/Medium/MediumDynamicProgrammingProblems.swift
@@ -181,6 +181,63 @@ extension MediumDynamicProgrammingProblems {
     }
 }
 
+// MARK: - 91. Decode Ways
+// LINK: https://leetcode.com/problems/decode-ways/
+//
+// Description: A message containing letters from A-Z can be encoded into numbers using the following mapping:
+// 'A' -> "1"
+// 'B' -> "2"
+// ...
+// 'Z' -> "26"
+// To decode an encoded message, all the digits must be grouped then mapped back into letters using the reverse of the
+// mapping above (there may be multiple ways). For example, "11106" can be mapped into:
+// "AAJF" with the grouping (1 1 10 6)
+// "KJF" with the grouping (11 10 6)
+// Note that the grouping (1 11 06) is invalid because "06" cannot be mapped into 'F' since "6" is different from "06".
+// Given a string s containing only digits, return the number of ways to decode it. The answer is guaranteed to fit in
+// a 32-bit integer.
+//
+// Strategy: Create a dp array that denotes the number of ways to decode first n characters. dp[0] = 1 because there is
+// only 1 way to decode empty str, because empty str yields nothing. dp[1] = 0 if the first char of s is 0, otherwise
+// dp[1] = 1 because if the first char of s is within (1,9), it will always be decoded to a specific alphabet. For the
+// case of dp[n], we need to consider if the last character can be decoded properly, if it can, then it would have at
+// least dp[n - 1] ways of decoding. Now we need to consider if the last 2 characters can be decoded properly, if they
+// can, then it would have at least another dp[n - 2] ways of decoding.
+//
+// Example: "1226"
+// "6" is decodable:
+// - (1)(2)(2)(6)
+// - (1)(22)(6)
+// - (12)(2)(6)
+// Notice it is really just adding 6 to all decodable ways of "122"
+// "26" is decodable:
+// - (1)(2)(26)
+// - (12)(26)
+// Notice it is really just adding 26 to all decodable ways of "12"
+
+extension MediumDynamicProgrammingProblems {
+    func numDecodings(_ s: String) -> Int {
+        var dp: [Int] = Array(repeating: 0, count: s.count + 1)
+        let intValues: [Int] = s.compactMap { $0.wholeNumberValue }
+        
+        dp[0] = 1
+        dp[1] = (intValues[0] == 0) ? 0 : 1
+        
+        for n in 2..<dp.count {
+            if intValues[n - 1] > 0 {
+                dp[n] += dp[n - 1]
+            }
+            
+            let lastTwoDigitValues: Int = (intValues[n - 2] * 10) + intValues[n - 1]
+            if lastTwoDigitValues >= 10 && lastTwoDigitValues <= 26 {
+                dp[n] += dp[n - 2]
+            }
+        }
+        
+        return dp[s.count]
+    }
+}
+
 // MARK: - 139. Word Break
 // LINK: https://leetcode.com/problems/word-break/
 // VIDEO: https://www.youtube.com/watch?v=Sx9NNgInc3A
